2fc835ce6be52dbaf276ee234cd586f35df23d2a
Anderson Santos <adnnskt@gmail.com>
2026-04-01 17:54:15 -0300

    add app.py

---
+++ spark-docker/apps/aplicativo.py
@@ -0,0 +1,6 @@
+from pyspark.sql import SparkSession 
+from pyspark.sql.functions import * 
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Aplicação.py").getOrCreate()
+    arqschema = 
