a65fe32af8dc9a65f02d3a04605f91ee00eb94da
Anderson Santos <adnnskt@gmail.com>
2026-04-07 18:30:33 -0300

    add sys package for py file

--- spark-docker/apps/.ipynb_checkpoints/aplicativo-checkpoint.py
+++ spark-docker/apps/.ipynb_checkpoints/aplicativo-checkpoint.py
@@ -1,10 +1,11 @@
+import sys
 from pyspark.sql import SparkSession 
 from pyspark.sql.functions import * 
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Aplicativo.py").getOrCreate()
     arqschema = "id INT, nome STRING, status STRING, cidade STRING, vendas INT, data STRING"
-    despachantes = spark.read.csv("dados/despachantes.csv", header = False, schema = arqschema)
+    despachantes = spark.read.csv(sys.argv[1], header = False, schema = arqschema)
     calculo = despachantes.select("data").groupBy(year("data")).count()
     calculo.write.format("console").save()
     spark.stop()
